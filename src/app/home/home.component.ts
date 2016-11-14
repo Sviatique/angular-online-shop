e990cfd835ebc73ba60b9d999c8b46c0e6118d6d
LEVI9\s.kharchevnyi <s.kharchevnyi@levi9.com>
2016-11-14 17:09:40 +0200

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by s.kharchevnyi on 10/11/2016.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './catalog.component.html',
+})
+export class CatalogComponent {
+
+}
